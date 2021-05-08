0b7738829d855a693e36205bec755e1231df2f8c
KIm <fenderst0612@gmail.com>
2021-05-08 18:23:41 +0900

    静的ファイルの表示 (http://localhost:3000/static/golang_gin.png)

--- main.go
+++ main.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	_ "fmt"
-	"net/http"
+	_ "net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
-	// htmlディレクトリ指定
-	engine.LoadHTMLGlob("templates/*")
-	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			// htmlに渡す変数を定義
-			"message": "hello gin",
-		})
-	})
+	engine.Static("/static", "./static")
 	engine.Run(":3000")
 }
