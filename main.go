0bdaf052366ddffc24b737a25d6159a90cfa81d2
KIm <fenderst0612@gmail.com>
2021-05-08 18:10:24 +0900

    HTMLファイルを扱う

--- main.go
+++ main.go
@@ -9,16 +9,12 @@ import (
 
 func main() {
 	engine := gin.Default()
-	ua := ""
-	// ミドルウェアを使用
-	engine.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
-		c.Next()
-	})
+	// htmlディレクトリ指定
+	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "hello world",
-			"User-Agent": ua,
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			// htmlに渡す変数を定義
+			"message": "hello gin",
 		})
 	})
 	engine.Run(":3000")
